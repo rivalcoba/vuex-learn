614b7e0e9771d85e9ceb2f594d92a8860c9240d7
rivalcoba <ivan.rivalcoba@gmail.com>
2021-08-25 18:18:27 -0500

    Cart Items and Total Done!!

--- src/main.js
+++ src/main.js
@@ -2,12 +2,16 @@ import Vue from "vue";
 import App from "./App.vue";
 // Importando el store
 import store from "@/store/index";
+// importing filter
+import { currency } from "@/helpers/currency";
 
 Vue.config.productionTip = false;
+// Registering filter globally
+Vue.filter("currency", currency);
 
 new Vue({
   // Inyectando el store dentro de
   // la raíz del componente
   store,
-  render: (h) => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
