305c3f375a84b250b54f8e39eb159a017fd102d0
rivalcoba <ivan.rivalcoba@gmail.com>
2021-09-09 13:28:33 -0500

    Dividir store en multiples archivos Done

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,8 @@
 // 1. Importando VUE y VUEX
 import Vuex from "vuex";
 import Vue from "vue";
-
-// 2. Importando la API simulada
-import shop from "@/api/shop";
+// Se importan las "actions"
+import actions from './actions'
 
 // 2. Registrando VUEX en VUE
 Vue.use(Vuex);
@@ -51,60 +50,7 @@ export default new Vuex.Store({
       };
     },
   },
-  actions: {
-    // Métodos
-    fetchProducts({ commit }) {
-      // retornando una promesa
-      return new Promise((resolve) => {
-        // Realizando la petición de los productos
-        shop.getProducts((products) => {
-          // Comitiando los datos recolectados
-          // store.commit(<mutation>, <payload>)
-          commit("setProducts", products);
-          resolve();
-        });
-      });
-    },
-    addProductToCart({state, getters, commit}, product) {
-      // El producto esta en existencia
-      if (getters.productIsInStock(product)) {
-        const cartItem = state.cart.find(
-          (item) => item.id === product.id
-        );
-        // Verificando si el item
-        // que se desea agregar no esta
-        // en el carrito
-        if (!cartItem) {
-          // Si no esta se agrega
-          commit("pushProductToCart", product.id);
-        } else {
-          // Si ya esta el item tan solo se incrementa
-          // la cantidad
-          commit("incrementItemQuantity", cartItem);
-        }
-        // Esta mutación se agrega cuando
-        // se desea cuidar que no se compren mas productos
-        // de los que se tienen disponibles en el inventario
-        commit("decrementProductInventory", product);
-      }
-    },
-    checkout({ commit, state }) {
-      shop.buyProducts(
-        // Proporciono el carrito de compras
-        state.cart,
-        // Callback Succesfully
-        () => {
-          // Comitearemos dos acciones
-          commit("emptyCart");
-          commit("setCheckoutStatus", "success");
-        },
-        // Callback Faill
-        () => {
-          commit("setCheckoutStatus", "fail");
-        }
-      );
-    },
-  },
+  actions,
   mutations: {
     // Actualiza el estado
     setProducts(state, /* payload */ products) {
